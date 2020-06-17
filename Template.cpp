170b363f5ed8c2998fa91c7f5f05691cecf59daa
vaibhav gautam <vaibhav.gautam2308@gmail.com>
2020-06-17 12:40:10 +0530

    Template Modified

--- Template.cpp
+++ Template.cpp
@@ -6,8 +6,10 @@ using namespace std;
 # define F first
 # define S second
 # define REP(i,a,b) for(int i=a;i<=b;i++)
+# define my_sizeof(type) ((char *)(&type+1)-(char*)(&type))
 using ll = long long int;
 const int MOD = 1e9 + 7;
+
 int main()
 {
 	ios::sync_with_stdio(false);
